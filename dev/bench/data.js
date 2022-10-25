6078cc3513f67991006ce2d6db5db255e4842ed6
github-action-benchmark <github@users.noreply.github.com>
2022-10-25 12:58:51 +0000

    add Go Benchmark (go) benchmark result for e96888998ae5883b5fdbbde549a850f4429bdf84

---
+++ dev/bench/data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1666702731905,
+  "repoUrl": "https://github.com/alpocketmonster/go-http-server",
+  "entries": {
+    "Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "epanechkina_av@nmlk.com",
+            "name": "epanechkina_av"
+          },
+          "committer": {
+            "email": "epanechkina_av@nmlk.com",
+            "name": "epanechkina_av"
+          },
+          "distinct": true,
+          "id": "e96888998ae5883b5fdbbde549a850f4429bdf84",
+          "message": "turn off logs",
+          "timestamp": "2022-10-25T15:58:11+03:00",
+          "tree_id": "024a6a86a1712bccf2baeb3d26a8a6369ea7e823",
+          "url": "https://github.com/alpocketmonster/go-http-server/commit/e96888998ae5883b5fdbbde549a850f4429bdf84"
+        },
+        "date": 1666702731053,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkTestHomepageHandler",
+            "value": 26938,
+            "unit": "ns/op\t   19579 B/op\t     216 allocs/op",
+            "extra": "39633 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
